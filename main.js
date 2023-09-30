816ba3559b6f9b9b3f902190533fc62dba811d9c
EF0601 <edwardfang0601@gmail.com>
2023-09-30 07:58:59 -0400

    Sept 30 update.
    Radio,
    MT Vinson Massif Base.

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ let gameValues = {
     health: 100,
     days: 0,
     milesAdvancedToday: 0,
+    coins: 100,
 
     mileToNext: 825,
 };
@@ -19,6 +20,7 @@ function updateValues() {
     document.getElementById("days").textContent = gameValues.days;
     document.getElementById("rations").textContent = gameValues.rations;
     document.getElementById("distance").textContent = gameValues.mileToNext;
+    document.getElementById("coins").textContent = gameValues.coins;
     if (shouldShowStats === true) {
         document.getElementById("stats").style.display = "block";
     }
@@ -36,7 +38,7 @@ function randomNumber(min, max) {
 }
 
 function randomEvent() {
-    if (randomNumber(1, 100) <= 100) {
+    if (randomNumber(1, 100) <= 20) {
         let event = events[randomNumber(0, events.length - 1)];
         switch (event) {
             case 'Lost':
@@ -76,6 +78,9 @@ function randomEvent() {
         }
         updateValues();
 
+    }
+    else{
+        document.querySelector("#event").textContent = "No random event today.";
     }
 }
 
@@ -122,13 +127,12 @@ let lockedSelection = 0;
 
 function handleSelection() {
     if (gamePart === 1) {
-        if (userSelection <= 3) {
+        if (userSelection <= 2) {
             switch (userSelection) {
                 case 1:
                     document.getElementById("gamePart1Selection1").style.color = "yellow";
 
                     document.getElementById("gamePart1Selection2").style.color = "white";
-                    document.getElementById("gamePart1Selection3").style.color = "white";
 
                     lockedSelection = 1;
                     break;
@@ -136,19 +140,9 @@ function handleSelection() {
                     document.getElementById("gamePart1Selection2").style.color = "yellow";
 
                     document.getElementById("gamePart1Selection1").style.color = "white";
-                    document.getElementById("gamePart1Selection3").style.color = "white";
 
                     lockedSelection = 2;
                     break;
-                case 3:
-                    document.getElementById("gamePart1Selection3").style.color = "yellow";
-
-                    document.getElementById("gamePart1Selection1").style.color = "white";
-                    document.getElementById("gamePart1Selection2").style.color = "white";
-
-                    lockedSelection = 3;
-                    break;
-
                 default:
                     break;
             }
@@ -233,6 +227,9 @@ function confirmSelection() {
 
                 gamePart = 2;
             }
+            if (lockedSelection === 2) {
+                window.open('https://www.google.com', '_blank').focus();
+            }
             break;
         case 2:
             switch (lockedSelection) {
@@ -265,6 +262,12 @@ function confirmSelection() {
                     if (gameValues.mileToNext <= 0) {
                         gamePart = 4;
                         document.getElementById("gamePart3").style.display = "none";
+                        document.getElementById("gamePart4").style.display = "block";
+                        lockedSelection = 0;
+                        gameValues.morale = 100;
+                        gameValues.health++;
+                        gameValues.mileToNext = 0;
+                        updateValues();
                     }
                     if (gameValues.morale <= 0) {
                         if (gameValues.health-5 <= 0) {
